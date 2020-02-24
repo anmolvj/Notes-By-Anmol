1fc2b0bea84f8a042cee0f700f1d4459974c260a
Anmol Vijayvargiya <anmol.vijay94@gmail.com>
2020-02-23 18:48:40 -0800

    update site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Anmol Vijayvargiya',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.'
+      "I am a classic millennial geek living in Silicon Valley who's love and appreciation of the great invention of the internet led him to follow the path of a full-stack software developer."
   },
   plugins: [
     'gatsby-plugin-react-helmet',

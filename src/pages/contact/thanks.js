4aa25aebe17d1f6331b19369ffb67cc05ce7fe78
Anmol Vijayvargiya <anmol.vijay94@gmail.com>
2020-04-06 19:13:13 -0700

    Update thanks.js

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,8 +6,8 @@ export default () => (
         <section className="section">
             <div className="container">
                 <div className="content">
-                    <h1>Thank you!</h1>
-                    <p>This is a custom thank you page for form submissions</p>
+                    <h1>🐶 Thank you!</h1>
+                    <p>I will get 🔙 to you as soon as possible.</p>
                 </div>
             </div>
         </section>

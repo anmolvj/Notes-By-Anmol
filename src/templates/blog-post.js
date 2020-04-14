d88511921741748485a3be365f0967401f33f7af
Anmol Vijayvargiya <anmol.vijay94@gmail.com>
2020-04-13 22:38:53 -0700

    set responsive image width's

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 import { graphql } from 'gatsby'
+import Paper from '@material-ui/core/Paper'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 import Tags from '../components/Tags'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
+const BookCoverContainer = styled(Paper)`
+    width: 70%;
+    margin: auto;
+    @media (min-width: 768px) {
+        width: 50%;
+    }
+    @media (min-width: 1024px) {
+        width: 35%;
+    }
+    @media (min-width: 1440px) {
+        width: 30%;
+    }
+`
+
+const TitleContainer = styled.div`
+    text-align: center;
+    margin-bottom: 20px;
+`
+const TitleHeader = styled.div`
+    font-size: 2rem;
+    font-weight: bold;
+`
+const TitleSubHeader = styled.div``
+
+const TextContainer = styled.div`
+    padding: 10px;
+    margin-top: 30px;
+`
+const Description = styled.div`
+    padding-bottom: 20px;
+    font-weight: bold;
+`
 export const BlogPostTemplate = ({
     content,
     contentComponent,
@@ -25,19 +59,27 @@ export const BlogPostTemplate = ({
             <div className="container content">
                 <div className="columns">
                     <div className="column is-10 is-offset-1">
-                        <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-                            {title}{' '}
-                            {author && <i className="subtitle">by {author}</i>}
-                        </h1>
-                        <p>{description}</p>
-                        <PreviewCompatibleImage imageInfo={featuredimage} />
-                        <PostContent content={content} />
-                        {tags && tags.length ? (
-                            <div style={{ marginTop: `4rem` }}>
-                                <h4>Tags</h4>
-                                {<Tags tags={tags} size="medium" />}
-                            </div>
-                        ) : null}
+                        <TitleContainer>
+                            <TitleHeader>{title}</TitleHeader>
+                            {author && (
+                                <TitleSubHeader>by {author}</TitleSubHeader>
+                            )}
+                        </TitleContainer>
+
+                        <BookCoverContainer elevation={15}>
+                            <PreviewCompatibleImage imageInfo={featuredimage} />
+                        </BookCoverContainer>
+                        <TextContainer>
+                            <Description>{description}</Description>
+
+                            <PostContent content={content} />
+                            {tags && tags.length ? (
+                                <div style={{ marginTop: `4rem` }}>
+                                    <h4>Tags</h4>
+                                    {<Tags tags={tags} size="medium" />}
+                                </div>
+                            ) : null}
+                        </TextContainer>
                     </div>
                 </div>
             </div>
@@ -104,7 +146,7 @@ export const pageQuery = graphql`
                 tags
                 featuredimage {
                     childImageSharp {
-                        fluid(maxWidth: 120, quality: 100) {
+                        fluid(maxWidth: 768, quality: 100) {
                             ...GatsbyImageSharpFluid
                         }
                     }
